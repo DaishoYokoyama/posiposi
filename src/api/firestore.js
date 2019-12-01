519013638289e1de9205e972320674221c5b5ba1
Daisho Yokoyama <daisyou117@gmail.com>
2019-12-01 15:32:48 +0900

    update api

--- src/api/firestore.js
+++ src/api/firestore.js
@@ -4,29 +4,28 @@ const firestore = firebase.firestore();
 const rooms = firestore.collection('rooms');
 const roomObjects = firestore.collection('roomObjects');
 
-// TODO 然るべき場所に移動
 const defaultFieldWidth = 800;
 const defaultFieldHeight = 800;
 
-export async function createRoom(roomId) {
-  return rooms.doc(roomId).set({
-    roomId,
-    fieldWidth: defaultFieldWidth,
-    fieldHeight: defaultFieldHeight,
+export async function createRoom(id) {
+  return rooms.doc(id).set({
+    id,
+    width: defaultFieldWidth,
+    height: defaultFieldHeight,
   });
 }
 
-export async function getRoomInfo(roomId) {
-  const doc = await rooms.doc(roomId).get();
+export async function getRoomInfo(id) {
+  const doc = await rooms.doc(id).get();
   return doc.exists ? doc.data() : null;
 }
 
-export async function getRoomObjectRef(roomId) {
-  return roomObjects.where('roomId', '==', roomId);
+export async function getRoomObjectRef(id) {
+  return roomObjects.where('id', '==', id);
 }
 
 export async function createRoomObject(object) {
-  return roomObjects.doc(object.objectId).set(object);
+  return roomObjects.doc(object.id).set(object);
 }
 
 export default {
